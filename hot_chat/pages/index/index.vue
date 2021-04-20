e94a4fa4d93a857959697056f3ff1d70f9de94d9
wt032320 <1476081727@qq.com>
2021-04-20 23:10:50 +0800

    项目搭建

---
+++ hot_chat/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="content">
+		<view class="top-bar">
+			<view class="top-bar-left">
+				
+			</view>
+			<view class="top-bar-center">
+				
+			</view>
+			<view class="top-bar-right">
+				
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+</style>
